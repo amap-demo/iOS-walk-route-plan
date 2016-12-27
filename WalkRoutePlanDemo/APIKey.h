8ddd6b0e7be8d0062d52d648bedfa6b31af4156c
wenan.lwa <wenan.lwa@alibaba-inc.com>
2016-12-27 18:47:04 +0800

    init

---
+++ WalkRoutePlanDemo/APIKey.h
@@ -0,0 +1,18 @@
+//
+//  APIKey.h
+//  WalkRoutePlanDemo
+//
+//  Created by eidan on 16/12/27.
+//  Copyright © 2016年 Amap. All rights reserved.
+//
+
+#ifndef APIKey_h
+#define APIKey_h
+
+/* 使用高德地图API，请注册Key，注册地址：http://lbs.amap.com/dev/#/ */
+
+const static NSString *APIKey = @"e0e3c1e3f58f5f0c4b2dbcc6e334abee";
+
+
+#endif
+
